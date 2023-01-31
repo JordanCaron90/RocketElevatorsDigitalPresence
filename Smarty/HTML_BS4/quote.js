72e0613ed967fa3f30ade10cca6a6a077cd1dbe4
jordan caron <jord.car199015@gmail.com>
2023-01-30 22:29:20 -0500

    js associer id

---
+++ Smarty/HTML_BS4/quote.js
@@ -0,0 +1,8 @@
+const hiThere = document.getElementById('number-of-apartements')
+hiThere.style.display = 'none'
+hit1 = document.getElementById('number-of-floors')
+hit1.style.display ='none'
+hit2 = document.getElementById('maximum-of-occupancy')
+hit2.style.display ='none'
+hit3 = document.getElementById('number-of-elevators')
+hit3.style.display ='none'
