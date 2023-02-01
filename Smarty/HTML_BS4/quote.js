b1aa249df17e983cdbd58e8c9e6dd15efc4d1a00
jordan caron <jord.car199015@gmail.com>
2023-02-01 13:57:23 -0500

    around

--- Smarty/HTML_BS4/quote.js
+++ Smarty/HTML_BS4/quote.js
@@ -9,6 +9,9 @@ hit3 = document.getElementById('number-of-elevators')
 hit3.style.display ='none'
 
 const dropdown = document.getElementById('dropdown')
+let app = document.getElementById('apartements-input').value;
+let floor = document.getElementById('floors-input').value;
+let occup = document.getElementById('occupancy-input').value;
 
 // Les value peuve etre
 // building type
@@ -23,16 +26,21 @@ dropdown.addEventListener('change', (event) => {
         hit1.style.display = ('block');
         hit2.style.display = ('none');
         hit3.style.display = ('none');
+        calculateResidential(app, floor);
+        document.getElementById('elevators-output').value = calculateResidential(app, floor);
     }  else if (dropdown.value =='commercial') {
         hit0.style.display = ('none');
         hit1.style.display = ('block');
         hit2.style.display = ('block');
         hit3.style.display = ('none');
+        calculateCommmercial(floor, occup);
+        document.getElementById('elevators-output').value = calculateCommmercial(floor, occup);
     }  else if (dropdown.value =='industrial') {
         hit0.style.display = ('none');
         hit1.style.display = ('none');
         hit2.style.display = ('none');
         hit3.style.display = ('block');
+        document.getElementById('elevators-output').value = calculateIndustrial();
     }  else if (dropdown.value =='buildingType') {
         hit0.style.display = ('none');
         hit1.style.display = ('none');
@@ -41,31 +49,44 @@ dropdown.addEventListener('change', (event) => {
     }
 })
 
-function calculateResidential() {
-    const app = document.getElementById('number-of-appartements').value;
-    const floor = document.getElementById('number-of-floors').value;
-    
+// residential.addEventListener('input')
+
+function calculateResidential(app, floor) {
+    app = document.getElementById('apartements-input').value;
+    floor = document.getElementById('floors-input').value;
+    console.log(app)
+    console.log(floor)
     const AppPerFloor = app/floor
-    const ascRequis = kkchose.Roundup (AppPerFloor/6)
+    console.log(AppPerFloor)
+    const ascRequis = AppPerFloor/6
+    console.log(ascRequis)
     const columnAditionel = floor/20
+    console.log('Column : ' + columnAditionel)
     const vraiAscenseurRequis = ascRequis * columnAditionel
-    document.getElementById('elevators-ouput').value = vraiAscenseurRequis;
-
+    console.log(vraiAscenseurRequis)
+    return vraiAscenseurRequis;
 }
-function calculateCommmercial() {
-    const floor = document.getElementById('numer-of-floors').value;
-    const occup = document.getElementById('maximum-occupancy').value;
 
+
+function calculateCommmercial(floor, occup) {
+    floor = document.getElementById('floors-input').value;
+    occup = document.getElementById('occupancy-input').value;
+    console.log(floor)
+    console.log(occup)
     const occupantMax = floor * occup
+    console.log(occupantMax)
     const ascRequis = occupantMax/200 
-    const coloneNumber = floors/10 
+    console.log(ascRequis)
+    const coloneNumber = floor/10 
+    console.log('Column')
     const vraiAscenseurRequis = ascRequis + coloneNumber
-    document.getElementById('elevators-output').value = vraiAscenseurRequis;
+    console.log(vraiAscenseurRequis)
+    return vraiAscenseurRequis
 
 }
 
 function calculateIndustrial() {
-    const ascRequis = document.getElementById('number-of-elevators').value;
+    const ascRequis = document.getElementById('number-of-elevators');
 
     const vraiAscenseurRequis = ascDemander
     const returnvalue = documment.getElementById('industrial-output').value = vraiAscenseurRequis;
